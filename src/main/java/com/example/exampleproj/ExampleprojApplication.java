327c10bf61b87c33ededd6bd715fe61b2e17a61e
Park SeungHyun <danbaram0420@korea.ac.kr>
2024-06-05 15:22:45 +0900

    first commit

---
+++ src/main/java/com/example/exampleproj/ExampleprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.exampleproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleprojApplication.class, args);
+	}
+
+}
